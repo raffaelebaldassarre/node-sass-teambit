2d7d1060bca216dd646afdb4e933b32e12a5c229
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-26 17:31:11 +0100

    NavBar

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,22 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: "#app",
+  data: {
+    nav_bar: {
+      img: "./dist/img/logo.svg",
+      link: ["Solutions", "Pricing", "Blog", "Sign in"],
+      button: {
+        img: "./dist/img/google-logo.svg",
+        text: "Sign Up"
+      }
+    },
+    section1: {
+      text: {}
+    }
+  }
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,24 @@
+let app = new Vue ({
+    el: "#app",
+    data : {
+        nav_bar : {
+            img : "./dist/img/logo.svg",
+            link : [
+                "Solutions",
+                "Pricing",
+                "Blog",
+                "Sign in",
+            ],
+            button : {
+                img: "./dist/img/google-logo.svg",                
+                text: "Sign Up"
+            }
+        },
+        section1 : {
+            text : {
+
+            }
+        }
+    }
+
+});
